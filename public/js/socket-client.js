8cc557244b8994d7d0abc58b44a3f8c1002660c0
Christian Marquez <marquezcristian920@gmail.com>
2023-01-11 21:52:04 -0700

    Realizando configuracion para mandar mensaje de estatus de conexion hacia el cliente

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,14 @@
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 const socket = io();
+
+socket.on('connect', () => {
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+});
