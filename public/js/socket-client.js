0a57114ddc619600e74b09b3a45e69f86bc82843
Christian Marquez <marquezcristian920@gmail.com>
2023-01-12 22:09:45 -0700

    Emitir desde el cliente y escuchar en el servidor

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,5 +1,7 @@
 const lblOnline = document.querySelector('#lblOnline');
 const lblOffline = document.querySelector('#lblOffline');
+const txtMesaje = document.querySelector("#txtMesaje");
+const btnEnviar = document.querySelector("#btnEnviar");
 
 const socket = io();
 
@@ -12,3 +14,13 @@ socket.on('disconnect', () => {
     lblOnline.style.display = 'none';
     lblOffline.style.display = '';
 });
+
+btnEnviar.addEventListener('click', () => {
+    const mesaje = txtMesaje.value;
+    const payload = {
+        msg: mesaje,
+        id: '1245',
+        fecha: new Date().getTime()
+    }
+    socket.emit('enviar-mensaje', payload );
+})
